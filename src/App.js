9a3ab61a66d6cf936e340ebc3aa4fac50ee87f2b
Spencer Hamilton <spencernhamilton@gmail.com>
2023-12-23 15:08:42 -0700

    yay

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ import SwimTeam from './components/SwimTeam';
 import SwimLessons from './components/SwimLessons';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from './components/Home';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyAFh9v1fwhfq1l5aR3z7wiKzycvfUoaRaM",
+  authDomain: "the-club-a7a51.firebaseapp.com",
+  projectId: "the-club-a7a51",
+  storageBucket: "the-club-a7a51.appspot.com",
+  messagingSenderId: "652245206951",
+  appId: "1:652245206951:web:e34cef62b8f3e322ee1882",
+  measurementId: "G-6MHD6QQTMS"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
 function App() {
   return (
     <NextUIProvider>
-      <Router>
+      <SwimLessons db={db} />
+      {/* <Router>
         <Switch>
           <Route exact path='/'>
             <Home />
@@ -22,7 +38,7 @@ function App() {
             <SwimLessons />
           </Route>
         </Switch>
-      </Router>
+      </Router> */}
     </NextUIProvider>
   );
 }
--- src/components/SwimLessons.jsx
+++ src/components/SwimLessons.jsx
@@ -1,15 +1,56 @@
 import React, { useState } from 'react';
-import { Grid, Input, Container, Text, Button, Spacer, Row } from '@nextui-org/react';
+import { Grid, Input, Container, Text, Button, Spacer, Row, Checkbox } from '@nextui-org/react';
 import { MdEmail, MdLocalPhone } from 'react-icons/md';
 import { GrSwim } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
 import { FaPlusCircle, FaMinusCircle } from 'react-icons/fa';
-const SwimLessons = () => {
+import validator from 'validator';
+import {addDoc, collection, doc, setDoc} from 'firebase/firestore';
+const SwimLessons = ({db}) => {
   {
     /* Beginner 1, 2, and 3, Parent and me  */
   }
   const [swimmerCount, setSwimmerCount] = useState(1);
-  const [swimmers, setSwimmers] = useState([{ firstName: '', lastName: '' }]);
+  const [swimmers, setSwimmers] = useState([{ firstName: '', lastName: '', age: 0 }]);
+  const [parentFirst, setParentFirst] = useState('');
+  const [parentLast, setParentLast] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [isMember, setIsMember] = useState(false);
+  const [groupLessons, setGroupLessons] = useState(false);
+  const [privateLessons, setPrivateLessons] = useState(false);
+
+  const validate = () => {
+    let valid = true;
+    swimmers.forEach(s => {
+      if (s.firstName === '' || s.lastName === '' || s.age === 0 || s.age === '') {
+        valid = false;
+      }
+    })
+    if (parentFirst === '' || parentLast === '') {
+      return false;
+    }
+    if (!validator.isEmail(email)) {
+      return false;
+    }
+    if (!validator.isMobilePhone(phone)) {
+      return false;
+    }
+    if (!valid) alert('Please ensure all swimmer fields are filled out')
+    return true;
+  }
+  const submit = async () => {
+    if (validate()) {
+        const res = await addDoc(collection(db, 'swim-lessons'), {
+          parentFirst,
+          parentLast,
+          email,
+          phone,
+          swimmers
+        })
+        console.log(res)
+    } else alert('Please fill out all fields')
+  }
 
   const addSwimmer = () => {
     setSwimmers([...swimmers, { firstName: '', lastName: '' }]);
@@ -30,12 +71,13 @@ const SwimLessons = () => {
 
   return (
     <>
-      <Container xs css={{ mt: 5 }}>
+      <Container sm css={{ mt: 5 }}>
         <Grid.Container justify='center' gap={1}>
           <Grid xs={12} justify='center'>
-            <Link to='/'>
+            {/* <Link to='/'>
               <GrSwim size={64} />
-            </Link>
+            </Link> */}
+            <GrSwim size={64} />
           </Grid>
           <Grid xs={12} justify='center'>
             <Text h2>Swim Lesson Signups</Text>
@@ -56,16 +98,10 @@ const SwimLessons = () => {
               </>
             </Row>
           </Grid>
-          {/* <Grid xs={6}>
-            <Input fullWidth placeholder='First Name' />
-          </Grid>
-          <Grid xs={6}>
-            <Input fullWidth placeholder='Last Name' />
-          </Grid> */}
           {swimmers.map((swimmer, index) => {
             return index === 0 ? (
               <React.Fragment key={index}>
-                <Grid xs={6}>
+                <Grid xs={5}>
                   <Input
                     id={index}
                     aria-label='First Name'
@@ -77,7 +113,7 @@ const SwimLessons = () => {
                     placeholder="Enter the swimmer's first name"
                   />
                 </Grid>
-                <Grid xs={6}>
+                <Grid xs={5}>
                   <Input
                     id={index}
                     fullWidth
@@ -85,15 +121,17 @@ const SwimLessons = () => {
                     aria-label='Last Name'
                     value={swimmers[index].lastName}
                     placeholder="Enter the swimmer's last name"
-                    // placeholder={`Swimmer #${index + 1} Last Name`}
                     onChange={(e) => updateSwimmers(e, index)}
                     name='lastName'
                   />
                 </Grid>
+                <Grid xs={2}>
+                  <Input type='number' fullWidth label='Age' placeholder='Age' value={swimmers[index].age} onChange={(e) => updateSwimmers(e, index)} name='age' />
+                </Grid>
               </React.Fragment>
             ) : (
               <React.Fragment key={index}>
-                <Grid xs={6}>
+                <Grid xs={5}>
                   <Input
                     id={index}
                     aria-label='First Name'
@@ -116,6 +154,9 @@ const SwimLessons = () => {
                   />
                 </Grid>
                 <Grid xs={1}>
+                <Input type='number' fullWidth placeholder='Age' value={swimmers[index].age} onChange={(e) => updateSwimmers(e, index)} name='age' />
+                </Grid>
+                <Grid xs={1}>
                   <Button
                     auto
                     color='error'
@@ -133,20 +174,34 @@ const SwimLessons = () => {
             <Text h4>Parent</Text>
           </Grid>
           <Grid xs={6}>
-            <Input fullWidth placeholder='First Name' />
+            <Input fullWidth placeholder='First Name' value={parentFirst} onChange={(e) => setParentFirst(e.target.value)} />
           </Grid>
           <Grid xs={6}>
-            <Input fullWidth placeholder='Last Name' />
+            <Input fullWidth placeholder='Last Name' value={parentLast} onChange={(e) => setParentLast(e.target.value)} />
           </Grid>
           <Grid xs={6}>
-            <Input fullWidth placeholder='Email' contentRight={<MdEmail />} />
+            <Input fullWidth placeholder='Email' contentRight={<MdEmail />} value={email} onChange={(e) => setEmail(e.target.value)} />
           </Grid>
           <Grid xs={6}>
-            <Input fullWidth placeholder='Phone' contentRight={<MdLocalPhone />} />
+            <Input fullWidth placeholder='Phone' contentRight={<MdLocalPhone />} value={phone} onChange={(e) => setPhone(e.target.value)} />
+          </Grid>
+          <Grid xs={12} justify='flex-start'>
+            <Text h4>What type of lessons are you interested in? (Select one or both)</Text>
+          </Grid>
+          <Grid xs={12} justify='flex-start'>
+            <Checkbox label='Group Lessons' value={groupLessons} onChange={(e) => setGroupLessons(e)} />
+            <Spacer x={4} />
+            <Checkbox label='Private Lessons' value={privateLessons} onChange={(e) => setPrivateLessons(e)} />
+          </Grid>
+          <Grid xs={12} justify='flex-start'>
+            <Text h4>Are you a member at The Club?</Text>
+          </Grid>
+          <Grid xs={12} justify='flex-start' >
+            <Checkbox label='Yes, I am a member at The Club' value={isMember} onChange={(e) => setIsMember(e)} />
           </Grid>
           <Spacer y={1} />
           <Grid xs={12} justify='center'>
-            <Button>Submit</Button>
+            <Button onClick={submit}>Submit</Button>
           </Grid>
         </Grid.Container>
       </Container>
